54efd4317011099d326ab5018ff169156551068c
Rad Cirskis <nad2000@gmail.com>
2018-05-30 11:13:44 +1200

    trying to get version w/o including pbr

--- w3c_validator/__init__.py
+++ w3c_validator/__init__.py
@@ -1,15 +1,15 @@
 """Validate HTML5 files."""
 
-__all__ = (
-    '__version__',
-    'version_info'
-)
+# __all__ = (
+#     '__version__',
+#     'version_info'
+# )
 
-from pbr.version import VersionInfo
+# from pbr.version import VersionInfo
 
-_v = VersionInfo('mock').semantic_version()
-__version__ = _v.release_string()
-version_info = _v.version_tuple()
+# _v = VersionInfo('mock').semantic_version()
+# __version__ = _v.release_string()
+# version_info = _v.version_tuple()
 
 # flake8: noqa
 from .validator import *
